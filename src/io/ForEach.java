d9a9333ecea098c92091bd606d290eca530069b2
rahul <rahul@192.168.1.67>
2025-08-08 16:04:23 +0530

    added one more option for the movie

--- src/io/ForEach.java
+++ src/io/ForEach.java
@@ -6,7 +6,7 @@ public class ForEach {
 	public static void main(String[] args) {
 		
 		List<String> movies = 
-				Arrays.asList("Kubera","Bahubali","Kingdon","Bussiness Man","Hanuman");
+				Arrays.asList("Kubera","Bahubali","Kingdon","Bussiness Man","Hanuman","robot");
 		movies.forEach(System.out::println);
 		movies.forEach(movie ->
 		System.out.println(movie));
